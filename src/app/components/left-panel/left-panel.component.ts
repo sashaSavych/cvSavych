fe14fdf7cef26cea9890020584ca469d15473694
sashaSavych <oleksandr.i.savych@gmail.com>
2018-03-27 22:23:30 +0300

    make first version of left panel

--- src/app/components/left-panel/left-panel.component.ts
+++ src/app/components/left-panel/left-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cv-left-panel',
   templateUrl: './left-panel.component.html',
-  styleUrls: ['./left-panel.component.css']
+  styleUrls: ['./left-panel.component.scss']
 })
 export class LeftPanelComponent implements OnInit {
 
