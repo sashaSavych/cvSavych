b7f30ae5020c5b62f65356ea1988b2ab4d07d1e6
sashaSavych <oleksandr.i.savych@gmail.com>
2018-07-10 21:58:20 +0300

    add github link

--- src/assets/globals.ts
+++ src/assets/globals.ts
@@ -1,6 +1,7 @@
 export const GLOBALS = {
   social: {
     linkedin: 'https://linkedin.com/in/oleksandrsavych/',
+    github: 'https://github.com/sashaSavych',
     facebook: 'https://facebook.com/profile.php?id=100007052293053',
     instagram: 'https://instagram.com/sasha_savych/'
   }
