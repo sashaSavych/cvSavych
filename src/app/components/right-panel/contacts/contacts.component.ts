118a5b5580f3f5b53db179856d7f6e3073fa5ae7
sashaSavych <respect2203@gmail.com>
2018-07-13 15:56:10 +0300

    add Contacts block

--- src/app/components/right-panel/contacts/contacts.component.ts
+++ src/app/components/right-panel/contacts/contacts.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cv-contact',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  contactDataLocalizationKeys: {
+    leftBlock: string[];
+    rightBlock: string[];
+  };
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
+    this.contactDataLocalizationKeys = {
+      leftBlock: ['name', 'dateOfBirth', 'address'],
+      rightBlock: ['email', 'phone', 'skype']
+    };
   }
-
 }
