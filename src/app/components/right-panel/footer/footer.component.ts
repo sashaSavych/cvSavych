02f6d534b4f4c9f2a08df3d5bdcdf19bda40086d
sashaSavych <oleksandr.i.savych@gmail.com>
2018-07-17 22:21:38 +0300

    rework styles of About section

--- src/app/components/right-panel/footer/footer.component.ts
+++ src/app/components/right-panel/footer/footer.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'cv-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  downloadPDF(): void {
+    this.httpClient.get('../../../assets/files/CV_Savych.pdf', {responseType: 'blob'})
+      .subscribe(
+        response => FileSaver.saveAs(new Blob([response]), 'CV_Savych.pdf'),
+        (error) => console.error(error)
+      );
   }
 
-  print(): void {
-    window.print();
+  openPDF(): void {
+    const link = document.createElement('a');
+    link.href = '../../../assets/files/CV_Savych.pdf';
+    link.setAttribute('target', '_blank');
+    link.click();
   }
+
 }
+
