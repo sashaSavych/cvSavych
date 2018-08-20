86877a36d07aed2eaac0833535bd07c65747b421
sashaSavych <oleksandr.i.savych@gmail.com>
2018-08-20 18:51:14 +0300

    remove unnecessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-import {AuthService} from './auth/auth.service';
-declare var google: any;
 
 @Component({
   selector: 'cv-root',
@@ -9,35 +6,9 @@ declare var google: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  slideValue = 100;
-  root = document.querySelector(':root');
-  overlays: any[];
+  constructor() {  }
 
-  constructor(private translateService: TranslateService,
-              public authService: AuthService) {
-    // translateService.setDefaultLang('en');
-    // authService.handleAuthentication();
-  }
-
-  ngOnInit() {
-    this.initOverlays();
-  }
-
-  setLanguage(newLang: string): void {
-    this.translateService.use(newLang);
-  }
-
-  onFontSizeChanged(newValue: number): void {
-    newValue /= 10;
-    console.log(newValue);
-    console.log(this.root);
-    this.root['style'].setProperty('--font-size', newValue + 'px');
-    // root.style.setProperty('--line-height', newValue + 'px');
-  }
-
-  initOverlays() {
-    // this.overlays = [new google.maps.Marker({position: {lat: 50.4501, lng: 30.5234}, title: 'Hometown'})];
-  }
+  ngOnInit() {  }
 
   onActivate(e, scrollContainer) {
     scrollContainer.scrollTop = 0;
--- src/app/services/helper.service.ts
+++
@@ -1,8 +0,0 @@
-export class HelperService {
-  constructor() {
-  }
-
-  goTop(element: HTMLElement): void {
-    element.scrollTo(0, 0);
-  }
-}
