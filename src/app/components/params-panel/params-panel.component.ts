397bf0f678d8b6cf77a65405b89e69afd6c27118
sashaSavych <oleksandr.i.savych@gmail.com>
2018-08-20 19:24:31 +0300

    hide DE language from switcher

--- src/app/components/params-panel/params-panel.component.ts
+++ src/app/components/params-panel/params-panel.component.ts
@@ -1,31 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'cv-params-panel',
   templateUrl: './params-panel.component.html',
-  styleUrls: ['./params-panel.component.css']
+  styleUrls: ['./params-panel.component.scss']
 })
 export class ParamsPanelComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor() { }
 
   ngOnInit() { }
-
-  downloadPDF(): void {
-    this.httpClient.get('../../../assets/files/CV_Savych.pdf', {responseType: 'blob'})
-      .subscribe(
-        response => FileSaver.saveAs(new Blob([response]), 'CV_Savych.pdf'),
-        (error) => console.error(error)
-      );
-  }
-
-  openPDF(): void {
-    const link = document.createElement('a');
-    link.href = '../../../assets/files/CV_Savych.pdf';
-    link.setAttribute('target', '_blank');
-    link.click();
-  }
 
 }
