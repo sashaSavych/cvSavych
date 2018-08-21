8df89b1f33f3cf4bed01a7ae66295e32917ace10
sashaSavych <respect2203@gmail.com>
2018-08-21 09:52:54 +0300

    extend Contact-item component

--- src/app/components/right-panel/contact-item/contact-item.component.ts
+++ src/app/components/right-panel/contact-item/contact-item.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ContactItemComponent implements OnInit {
   @Input() iconName = '';
   @Input() value = '';
+  @Input() title = '';
 
   constructor() { }
 
